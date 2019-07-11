b7f8a23d756ac805729a8d23e3dbbaa32509dc3b
Infoskirmish <rickballard@infoskirmish.com>
2019-07-11 10:02:52 -0500

    First Commit

---
+++ ImaegHelper.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Drawing;
+
+namespace MalSocail
+{
+    public enum Channel
+    {
+        R,
+        B,
+        G,
+        A,
+        QT
+    }
+
+
+    /*
+        The human eye is fairly good at seeing small differences in brightness over a relatively large area, but not so good at distinguishing the exact strength of a high frequency 
+        (rapidly varying) brightness variation. This fact allows one to reduce the amount of information required by ignoring the high frequency components. This is done by simply 
+        dividing each component in the frequency domain by a constant for that component, and then rounding to the nearest integer. This is the main lossy operation in the whole process. 
+        As a result of this, it is typically the case that many of the higher frequency components are rounded to zero, and many of the rest become small positive or negative numbers.
+        
+        As human vision is also more sensitive to luminance than chrominance, further compression can be obtained by working in a non-RGB color space which separates the two (e.g., YCbCr), 
+        and quantizing the channels separately.
+
+        Wikipedia (https://en.wikipedia.org/wiki/Quantization_(image_processing)
+     
+     */
+
+    public static class ImaegHelper
+    {
+        //A quantization matrix
+        public static double[,] QT = new double[8, 8] {
+                                        { 06, 04, 04, 06, 10, 16, 20, 24 },
+                                        { 05, 05, 06, 08, 10, 23, 24, 22 },
+                                        { 06, 05, 06, 10, 16, 23, 28, 22 },
+                                        { 06, 07, 09, 12, 20, 35, 32, 25 },
+                                        { 07, 09, 15, 22, 27, 44, 41, 31 },
+                                        { 10, 14, 22, 26, 32, 42, 45, 37 },
+                                        { 20, 26, 31, 35, 41, 48, 48, 40 },
+                                        { 29, 37, 38, 39, 45, 40, 41, 40 }
+                                    };
+
+        //This method takes two 8x8 quantized matrices and prints a side by side comparison. Differnces are highlighted in red.
+        public static void PrintGridComparison(double[,] grid1, double[,] grid2, string name1, string name2, Channel channel)
+        {
+            if (grid1.Length != grid2.Length)
+                return;
+
+            int width =  (Math.Sqrt(grid1.Length) % 1) == 0 ? (int)Math.Sqrt(grid1.Length) : 0;
+            int height = (Math.Sqrt(grid1.Length) % 1) == 0 ? (int)Math.Sqrt(grid1.Length) : 0;
+
+            string terminal = "\t " + new String('-', width*height + 9) + "\n";
+            Console.Write(terminal);
+            if (channel == Channel.R)
+                Console.ForegroundColor = ConsoleColor.Red;
+            else if (channel == Channel.B)
+                Console.ForegroundColor = ConsoleColor.Blue;
+            else if (channel == Channel.G)
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("\t |Channel:  " + channel.ToString());
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("   " + name1 + "\t  |\t|Channel:  " + channel.ToString()+ " " + name2 + "\t |\n");
+            Console.Write("\t |");
+            for (int y = 0; y < height; y++)
+                Console.Write(string.Format("{0,3} ", y));
+            Console.Write("|\t|");
+            for (int y = 0; y < height; y++)
+                Console.Write(string.Format("{0,3} ", y));
+
+            Console.Write("|\n");
+            Console.Write(terminal);
+            for (int y = 0; y < height; y++)
+            {
+                Console.Write("\t" + y + "|");
+                for (int x = 0; x < width; x++)
+                {
+                    if(grid1[x,y] != grid2[x,y])
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.Write(string.Format("{0,3} ", grid1[x, y]));
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.Write("|\t|");
+                for (int x = 0; x < width; x++)
+                {
+                    if (grid1[x, y] != grid2[x, y])
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.Write(string.Format("{0,3} ", grid2[x, y]));
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.Write("|" + y + "\n");
+            }
+            Console.Write(terminal);
+        }
+
+        // A reuseable method to highlight different values in red
+        public static void ReadString(Bitmap image)
+        {
+           Color c = image.GetPixel(0, 0);
+           Color c2 = image.GetPixel(1, 0);
+            
+            string str = Convert.ToChar(c.R).ToString() + Convert.ToChar(c.G).ToString() + Convert.ToChar(c.B).ToString() + Convert.ToChar(c2.R).ToString() + Convert.ToChar(c2.G).ToString() + Convert.ToChar(c2.B).ToString();
+            Console.WriteLine(str);
+        }
+
+        //Extract hidden text from image
+        public static string ExtractString(Bitmap image, int x=0,int y = 0)
+        {
+            string extractedText = "";
+
+            int terminating = 0;
+            int counter = 0;
+
+            for (int i = y; i < image.Height; i++)
+            {
+                for (int j = x; j < image.Width; j++)
+                {
+                    if (counter % 15 == 0)
+                    {
+                        Color pixel = image.GetPixel(j, i);
+                        int RValue = pixel.R;
+                        int GValue = pixel.G;
+                        int BValue = pixel.B;
+                        if (RValue + GValue + BValue == 0)
+                        {
+                            terminating++;
+                            if (terminating == 8)
+                            {
+                                return extractedText;
+                            }
+                        }
+                        else
+                        {
+
+                            char r = (char)RValue;
+                            char g = (char)GValue;
+                            char b = (char)BValue;
+
+                            extractedText += r.ToString();
+                            extractedText += g.ToString();
+                            extractedText += b.ToString();
+
+                            terminating = 0;
+                        }
+                    }
+                }
+            }
+            return "";
+        }
+
+        // Insert string into image
+        public static Bitmap ImplantString(Bitmap image, string str, int x=0, int y=0)
+        {
+            int length = str.Length;
+            int index = 0;
+            int terminating = 0;
+            int R = 0, G = 0, B = 0;
+            int counter = 0;
+
+            for (int i = y; i < image.Height; i++)
+            {
+                for (int j = x; j < image.Width; j++)
+                {
+                    if (index < length)
+                    {
+                        if (counter % 15 == 0)
+                        {
+                            Color pixel = image.GetPixel(j, i);
+
+                            R = pixel.R;
+                            G = pixel.G;
+                            B = pixel.B;
+
+                            R = index < length ? R = str[index++] : 0;
+                            G = index < length ? G = str[index++] : 0;
+                            B = index < length ? B = str[index++] : 0;
+
+                            image.SetPixel(j, i, Color.FromArgb(R, G, B));
+                        }
+                    }
+
+                    else
+                    {
+                        if (terminating == 8)
+                        {
+                            return image;
+
+                        }else
+                        {
+                            image.SetPixel(j, i, Color.FromArgb(0, 0, 0));
+                            terminating++;
+                        }
+                    }
+                }
+            }
+            return image;
+        }
+
+
+        // Prints a single 8x8 Matrix
+        public static void PrintChannel(double[,] grid, Channel channel)
+        {
+            int width = (Math.Sqrt(grid.Length) % 1) == 0 ? (int)Math.Sqrt(grid.Length) : 0;
+            int height = (Math.Sqrt(grid.Length) % 1) == 0 ? (int)Math.Sqrt(grid.Length) : 0;
+
+            string[] lines = new string[height];
+
+            for (int y = 0; y < height; y++)
+            {
+                object[] obj = new object[width];
+                for (int x = 0; x < width; x++)
+                {
+                    obj[x] = grid[x, y];
+                }
+                string line = String.Format("\t| [{0,3} {1,3} {2,3} {3,3} {4,3} {5,3} {6,3} {7,3}] |\n",obj);
+                lines[y] = line;
+            }
+
+            string terminal = "\t" + new String('-', lines[0].Length -2) + "\n";
+            Console.Write(terminal);
+            if(channel == Channel.R)
+                Console.ForegroundColor = ConsoleColor.Red;
+            else if (channel == Channel.B)
+                Console.ForegroundColor = ConsoleColor.Blue;
+            else if (channel == Channel.G)
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("\t| Channel: " + channel.ToString() + "\t\t\t    |\n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            foreach (string line in lines)
+                Console.Write(line);
+            Console.Write(terminal);
+        }
+
+        //Quantize a 8x8 grid
+        public static double[,] Quantiz(double[,] grid)
+        {
+            int width = (Math.Sqrt(grid.Length) % 1) == 0 ? (int)Math.Sqrt(grid.Length) : 0;
+            int height = (Math.Sqrt(grid.Length) % 1) == 0 ? (int)Math.Sqrt(grid.Length) : 0;
+
+            double[,] block = new double[width, height];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    block[x, y] = Math.Round(grid[x, y] / QT[x, y]);
+                }
+            }
+            return block;
+        }
+
+        // Calculate the discrete cosine transform (DCT) of a quantized 8x8 block
+        public static double[,] GetIDCTBlock(double[,] grid)
+        {
+            int width = (Math.Sqrt(grid.Length) % 1) == 0 ? (int) Math.Sqrt(grid.Length) : 0;
+            int height = (Math.Sqrt(grid.Length) % 1) == 0 ? (int)Math.Sqrt(grid.Length) : 0;
+
+            double[,] block = new double[width,height];
+            int cell = 0;
+            double[] flatten = new double[width * height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    flatten[cell] = grid[x,y];
+                    cell++;
+                }
+            }
+
+            double[] vector = InverseTransform(flatten);
+            cell = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    block[x,y] = flatten[cell];
+                    cell++;
+                }
+            }
+            return block;
+        }
+
+        // Get 8x8 block from image
+        public static double[,] Get8by8(Bitmap image,Channel channel, int x = 0, int y = 0, int height = 8, int width = 8)
+        {
+            double[,] block = new double[width,height];
+
+            for (int yi = 0; yi < height; yi++)
+            {
+                for (int xi = 0; xi < width; xi++)
+                {
+                    Color pixel = image.GetPixel((x + xi), (y + yi));
+                    switch (channel)
+                    {
+                        case Channel.A:
+                            block[xi, yi] = pixel.A;
+                            break;
+                        case Channel.B:
+                            block[xi, yi] = pixel.B;
+                            break;
+                        case Channel.G:
+                            block[xi, yi] = pixel.G;
+                            break;
+                        case Channel.R:
+                            block[xi, yi] = pixel.R;
+                            break;
+                    }
+                }
+            }
+            return block;
+        }
+
+        // Get the image's first 8x8 block
+        public static void PrintFirstRowRBGValues(Bitmap image)
+        {
+            int width = 0;
+            int height = 0;
+
+            for (height = 0; height < 8; height++)
+            {
+                for (width = 0; width < 8; width++)
+                {
+                    Color pixel = image.GetPixel(width, height);
+                   
+                    Console.Write( String.Format("[{0,3} {1,3} {2,3} {3,3}]", pixel.R, pixel.B, pixel.G, pixel.A));
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("\n\t"+image.PixelFormat.ToString());
+            Console.WriteLine("\n\tWidth = " + width);
+            Console.WriteLine("\tHeight = " + height);
+        }
+
+        // Preform and inverse transform
+        public static double[] InverseTransform(double[] vector)
+        {
+            if (vector == null)
+                throw new NullReferenceException();
+            double[] result = new double[vector.Length];
+            double factor = Math.PI / vector.Length;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                double sum = vector[0] / 2;
+                for (int j = 1; j < vector.Length; j++)
+                    sum += vector[j] * Math.Cos(j * (i + 0.5) * factor);
+                result[i] = sum;
+            }
+            return result;
+        }
+        public static Bitmap CreateNonIndexedImage(Image src)
+        {
+            Bitmap newBmp = new Bitmap(src.Width, src.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            using (Graphics gfx = Graphics.FromImage(newBmp))
+            {
+                gfx.DrawImage(src, 0, 0);
+            }
+
+            return newBmp;
+        }
+
+        public static int reverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+
+namespace MalSocail
+{
+    class Program
+    {
+        private static readonly string KEY                      = "password";                                          //  Password to encrypt data with.
+        private static readonly string PathOriginalImage        = @"C:\Users\Public\clean.gif";                        //  Location of clean image with no encoded data.
+        private static readonly string TweetImageURL            = "https://pbs.twimg.com/media/D_M5VvWVAAAYIEx.png";   //  The URL of the tweet /w image
+        private static readonly string text                     = "I am a string hidden, and I came from an image";    //  The string to encrypt and hide inside an dimage.
+        private static readonly string TweetImageExfiledFile    = @"C:\Users\Public\secret_bannercopy.docx";           //  The file to save the extracted binary data.
+        private static readonly string TweetImageFile           = @"C:\Users\Public\banner_stuffed.gif";               //  The filename to save the download Twitter image to.
+        private static readonly string ImageWithHiddenString    = @"C:\Users\Public\string_stuffed.gif";               //  Image which contains a hidden encrypted string.
+        private static readonly string BinaryDataToHide         = @"C:\Users\Public\secret.docx";                      //  The binary file we wish to encrypt and stuff into an image.
+        private static readonly string ExfiledBinaryData        = @"C:\Users\Public\secret_copy.docx";                 //  The file/location where we will store the decrypted binary data
+        private static readonly string ImageWithDatafile        = @"C:\Users\Public\data_stuffed.gif";                 //  The image we will create which contains the encrypted binary data
+
+    static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("------------------[  End Testing Text String Exfil  ]------------------\n");
+
+            Bitmap image = GetImage(@"C:\Users\Public\clean.gif");                          //  Get clean image
+            string ciphertext = EncryptString(text, KEY);                                   //  Encrypt string/message we want to hide
+            Bitmap stuffed = ImaegHelper.ImplantString(image, ciphertext);                  //  Encode encrypted string into the image
+            SaveImage(stuffed, ImageWithHiddenString);                                      //  Save the newly created image with hidden string
+            Bitmap image_withHiddenString = GetImage(ImageWithHiddenString);                //  Get a fresh copy of the image we just saved
+            string extractedciphertext = ImaegHelper.ExtractString(image_withHiddenString); //  Extract string from encoded image file
+            Compare(ciphertext, extractedciphertext, "Encrypted strings");                  //  Compare the extracted encrypted string with the string
+                                                                                            //  we encrypted above.
+            string cleartext = DecryptString(extractedciphertext);                          //  Decypt encrypted string
+            Compare(text, cleartext, "Clear text strings");                                 //  Compare the decrypted string with the orginal string
+
+            Console.WriteLine("------------------[  End Testing Text String Exfil  ]------------------\n");
+
+
+
+            Console.WriteLine("------------------[ Start Create Image with Encoded Binary File ]------------------\n");
+
+            Bitmap file_image      = GetImage(PathOriginalImage);                           //  Get clean image 
+            string file_data       = ReadDataFile(BinaryDataToHide);                        //  Get data file to hide
+            string file_ciphertext = EncryptString(file_data, KEY);                         //  Encrypt file data and return a base64encoded string
+            Bitmap file_stuffed    = ImaegHelper.ImplantString(file_image, file_ciphertext);//  Insert encrypted data into the clean image
+            SaveImage(file_stuffed, ImageWithDatafile);                                     //  Save the newly created image
+            Bitmap image_withHiddenData = GetImage(ImageWithDatafile);                      //  Get a fresh copy of the image we just saved
+            string fileimage_ciphertext = ImaegHelper.ExtractString(image_withHiddenData);  //  Extract hidden file data from image
+            Compare(file_ciphertext, fileimage_ciphertext, "Encrypted strings");            //  For testing, compare the extracted text downloaded from Twitter to 
+                                                                                            //  what is expected based on the data string we encrypted above.
+            string file_exfildata = DecryptString(fileimage_ciphertext);                    //  Decrypt the exfiled binary data.
+            Compare(file_data, file_exfildata, "Base64Encoded strings");                    //  For testing, compare the decrypted base64 exfiled data string to the base64
+                                                                                            //  we encoded above.
+            WriteDataFile(file_exfildata, ExfiledBinaryData);                               //  Save the decrypted, exfiled binary data to a file.
+
+            Console.WriteLine("------------------[  End Binary File Exfil From Image   ]------------------\n");
+
+
+
+            Console.WriteLine("------------------[ Start Get Banner From Twitter and Decode Data ]------------------\n");
+
+            Bitmap banner_image = GetBannerImage(TweetImageURL);                        //  Download the image from Twitter
+            SaveImage(banner_image, TweetImageFile);                                    //  Save the downloaded image
+            Bitmap image_fromTwitter = GetImage(TweetImageFile);                        //  Get a fresh copy of the image we just saved
+            string banner_ciphertext = ImaegHelper.ExtractString(image_fromTwitter);    //  Extract the encrypted data
+            string banner_exfildata = DecryptString(banner_ciphertext);                 //  Decrypt the extracted data
+            Compare(file_data, banner_exfildata, "Base64Encoded strings");              //  Compare the decrypted data with the orginal data from above
+            WriteDataFile(banner_exfildata, TweetImageExfiledFile);                     //  Save the decrypte data to a file.
+            Print8by8ComparisonBlock(PathOriginalImage, TweetImageFile);                //  Print some pixel by pixel diagnostic data.
+
+            Console.WriteLine("\n------------------[  End Get Banner From Twitter and Decode Data  ]------------------\n");
+
+        }
+        private static void Compare(byte[] str1, byte[] str2, string title)
+        {
+            Compare(Convert.ToBase64String(str1), Convert.ToBase64String(str2), title);
+        }
+
+        private static void Compare(string str1, string str2, string title)
+        {
+            if (str1 == str2)
+            {
+                Console.WriteLine("\t" + title + "\tMATCH!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\t" + title + "\tDO NOT MATCH!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("String 1 = \t" + str1.Substring(0, Math.Min(str1.Length, 100)) + " \nString 2 = \t" + str2.Substring(0, Math.Min(str2.Length, 100)));
+            }
+        }
+
+        private static void Print8by8ComparisonBlock(string image1path, string image2path)
+        {
+            Bitmap image1 = GetImage(image1path);
+            Bitmap image2 = GetImage(image2path);
+
+            ImaegHelper.PrintFirstRowRBGValues(image1);
+            ImaegHelper.PrintFirstRowRBGValues(image2);
+
+            var image1_Rblock = ImaegHelper.Get8by8(image1, Channel.R, 8, 8);
+            var image1_Rdct   = ImaegHelper.GetIDCTBlock(image1_Rblock);
+            var image1_RQ     = ImaegHelper.Quantiz(image1_Rdct);
+
+            var image1_Bblock = ImaegHelper.Get8by8(image1, Channel.B, 8, 8);
+            var image1_Bdct   = ImaegHelper.GetIDCTBlock(image1_Bblock);
+            var image1_BQ     = ImaegHelper.Quantiz(image1_Bdct);
+
+            var image1_Gblock = ImaegHelper.Get8by8(image1, Channel.G, 8, 8);
+            var image1_Gdct   = ImaegHelper.GetIDCTBlock(image1_Gblock);
+            var image1_GQ     = ImaegHelper.Quantiz(image1_Gdct);
+
+            var image2_Rblock = ImaegHelper.Get8by8(image2, Channel.R, 8, 8);
+            var image2_Rdct   = ImaegHelper.GetIDCTBlock(image2_Rblock);
+            var image2_RQ     = ImaegHelper.Quantiz(image2_Rdct);
+
+            var image2_Bblock = ImaegHelper.Get8by8(image2, Channel.B, 8, 8);
+            var image2_Bdct   = ImaegHelper.GetIDCTBlock(image2_Bblock);
+            var image2_BQ     = ImaegHelper.Quantiz(image2_Bdct);
+
+            var image2_Gblock = ImaegHelper.Get8by8(image2, Channel.G, 8, 8);
+            var image2_Gdct   = ImaegHelper.GetIDCTBlock(image2_Gblock);
+            var image2_GQ     = ImaegHelper.Quantiz(image2_Gdct);
+
+            ImaegHelper.PrintGridComparison(image1_Rblock, image2_Rblock, "Image 1R",     "Image 2R",     Channel.R);
+            ImaegHelper.PrintGridComparison(image1_Rdct, image2_Rdct,     "Image1 RDCT",  "Image2 RDCT",  Channel.R);
+            ImaegHelper.PrintGridComparison(image1_RQ, image2_RQ,         "Image1 RQ",    "Image2 RQ",    Channel.R);
+            ImaegHelper.PrintGridComparison(image1_Bblock, image2_Bblock, "Image 1B",     "Image 2B",     Channel.B);
+            ImaegHelper.PrintGridComparison(image1_Bdct, image2_Bdct,     "Image1 BDCT",  "Image2 BDCT",  Channel.B);
+            ImaegHelper.PrintGridComparison(image1_BQ, image2_BQ,         "Image1 BQ",    "Image2 BQ",    Channel.B);
+            ImaegHelper.PrintGridComparison(image1_Gblock, image2_Gblock, "Image 1G",     "Image 2G",     Channel.G);
+            ImaegHelper.PrintGridComparison(image1_Gdct, image2_Gdct,     "Image1 GDCT",  "Image2 GDCT",  Channel.G);
+            ImaegHelper.PrintGridComparison(image1_GQ, image2_GQ,         "Image1 GQ",    "Image2 GQ",    Channel.G);
+        }
+
+        private static void WriteDataFile(byte[] encodedcontents, string topath)
+        {
+            File.WriteAllBytes(topath, encodedcontents);
+        }
+
+        private static void WriteDataFile(string encodedcontents,string topath)
+        {
+            byte[] bytes = Convert.FromBase64String(encodedcontents);
+            File.WriteAllBytes(topath, bytes);
+        }
+
+        private static string ReadDataFile(string filepath)
+        {
+            byte[] bytes = File.ReadAllBytes(filepath);
+            string bytesencoded = Convert.ToBase64String(bytes);
+            return bytesencoded;
+        }
+
+        private static void SaveImage(Bitmap image, string path)
+        {
+            image.Save(path);
+        }
+
+        private static Bitmap GetImage(string path)
+        {
+            Bitmap originalImage = ImaegHelper.CreateNonIndexedImage(Image.FromFile(path));
+            return originalImage;
+        }
+
+        private static string EncryptString(string plaintext, string key)
+        {
+            string encryptedData = Cipher.Encrypt(plaintext, key);
+            return encryptedData;
+        }
+
+        private static string DecryptString(string ciphertext)
+        {
+            string decryptedData = Cipher.Decrypt(ciphertext, KEY);
+            return decryptedData;
+        }
+
+        public static Bitmap GetBannerImage(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                using (Stream stream = client.OpenRead(url))
+                {
+                    Bitmap bitmap; bitmap = new Bitmap(stream);
+
+                    if (bitmap != null)
+                        return bitmap;
+                    return null;
+                }
+            }
+        }
+    }
+}
